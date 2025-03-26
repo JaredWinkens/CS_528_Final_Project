419077f75270c87d26dff49b08ed251954d4c38c
dec-xii <declan0wayman@gmail.com>
2025-03-26 16:38:41 -0400

    Basic vertically text boxes

--- hello-world-gtk.c
+++ hello-world-gtk.c
@@ -1,21 +1,34 @@
 #include <gtk/gtk.h>
+#include <gtk/gtklayoutmanager.h>
+#include <gtk/gtkshortcut.h>
 
-static void print_hello(GtkWidget *widget, gpointer data) {
-  g_print("Hello World\n");
-}
+#define APP_NAME "AI Chat"
 
 static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window;
-  GtkWidget *button;
+  GtkWidget *chat_box = NULL;
+  GtkWidget *input_box;
+  GtkTextBuffer *chat_buffer;
 
   window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Hello");
+  gtk_window_set_title(GTK_WINDOW(window), APP_NAME);
   gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
-  button = gtk_button_new_with_label("Hello World");
-  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-  gtk_window_set_child(GTK_WINDOW(window), button);
+  GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 3);
 
+  // Define Chat View Window
+  chat_box = gtk_text_view_new();
+  chat_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(chat_box));
+  gtk_text_buffer_set_text(chat_buffer, "TEST", -1);
+  gtk_box_append(GTK_BOX(vbox), chat_box);
+
+  // Define User data entry window
+  input_box = gtk_text_view_new();
+  gtk_box_append(GTK_BOX(vbox), input_box);
+
+  gtk_widget_set_visible(vbox, true);
+
+  gtk_window_set_child(GTK_WINDOW(window), vbox);
   gtk_window_present(GTK_WINDOW(window));
 }
 
@@ -23,7 +36,7 @@ int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new("org.gtk.ai_chat", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
